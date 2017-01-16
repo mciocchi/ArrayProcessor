99a477fa4590ecfe24ac3ed76a7724b780bbd638
Matt Ciocchi <mciocchi@users.noreply.github.com>
2017-01-16 15:24:25 -0500

    keep namespace clean, use better name for var

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var arrayProcessor = require('./ArrayProcessor').construct();
-
 // standard AWS Lambda convention - accept event and context objects
 exports.handler = function (event, context) {
-    var retval = arrayProcessor.process(event.input);
-    console.log(retval);
+    var arrayProcessor = require('./ArrayProcessor').construct();
+    var result = arrayProcessor.process(event.input);
+    console.log(result);
 };
 
 
